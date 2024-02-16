db0f6d6ccaf0319b636f0a45bdf274f9c499c6ca
Maxim Ustin <mxustin@gmail.com>
2024-02-16 17:08:22 +0300

    Реализовано наблюдение «Фактической версии» за изменениями входящих в ее состав «краткой» и «полной» (и тесты)

--- src/entities/protocol_version.py
+++ src/entities/protocol_version.py
@@ -1,69 +1,108 @@
 """ Версия протокола. Спецификация: https://clck.ru/38jK47 """
 
+from abc import ABC, abstractmethod
 from multipledispatch import dispatch
 
 from src.ground import errs
 from src.ground import bits
 
 
+def _undefined_version() -> tuple[int, int, int]:
+    """ Возвращает кортеж из трех «-1» для того, чтобы пометить «мажор», «минор» и «патч» как неопределенные """
+    return -1, -1, -1
+
+
+class ProtocolVersionObserver(ABC):
+    @abstractmethod
+    def process_changing(self):
+        ...
+
+
 class ProtocolVersionShort:
-    """ Краткая версия протокола (мажор — 2 бита, минор — 3 бита, патч — 3 бита) для «малого» заголовка.
-    Данная версия, по сути, является смещением по отношению к «Полной версии» """
+    """ «Краткая версия» протокола (используется «Семантическое версионирование»: «мажор» — 2 бита, «минор» — 3 бита,
+    «патч» — 3 бита) для «малого» заголовка. Данная версия, по сути, является смещением по трем параметрам по
+    отношению к «Полной версии» (см. ниже) """
 
     @dispatch(int, int, int)
-    def __init__(self, major: int = 0, minor: int = 0, patch: int = 0):
-        """ Инициализация краткой версии через три целых числа: "мажор", "минор", "патч", которые нужно
-        рассматривать как "сдвиг" по отношению к значениям последней актуальной полной версии """
-        self._major: int = -1  # Признак того, что значение не установлено
+    def __init__(self, major: int = 0, minor: int = 0, patch: int = 0) -> None:
+        """
+        Инициализация «Краткой версии» через три целых числа: «мажор» (0..3), «минор» (0..7), «патч» (0..7),
+        которые нужно рассматривать как «смещения» по отношению к соответствующим значениям последней
+        актуальной «Полной версии»
+        :param major: «Мажор»
+        :param minor: «Минор»
+        :param patch: «Патч»
+        """
+        # region Устанавливаем «неопределенные» значения
+        self._major, self._minor, self._patch = _undefined_version()
+        # endregion
+        # region Обозначаем «наблюдателя»
+        self._observer = None
+        self._observer: ProtocolVersionObserver
+        # endregion
+        # region Устанавливаем значения с соответствующими проверками
         self.major = major
-        self._minor: int = -1  # Признак того, что значение не установлено
         self.minor = minor
-        self._patch: int = -1  # Признак того, что значение не установлено
         self.patch = patch
+        # endregion
 
     @dispatch(bytearray)
     def __init__(self, raw_bytes: bytearray) -> None:
-        """ Инициализация через чтение "сырых" байтов (должен поступить байтовый массив из одного байта, где
-        2 бита - мажор, 3 бита - минор, 3 бита - патч, которые нужно рассматривать как сдвиг по отношению к
-        значениям последней актуальной версии) """
+        """ Инициализация через чтение «сырых» байтов (должен поступить байтовый массив из одного байта, где
+        2 бита — «мажор», 3 бита — «минор», 3 бита — «патч», которые нужно рассматривать как «смещения» по отношению к
+        соответствующим значениям последней актуальной «Полной версии») """
+        # region Обозначаем «наблюдателя»
+        self._observer = None
+        self._observer: ProtocolVersionObserver
+        # endregion
         if len(raw_bytes) == 1:
-            self._major = raw_bytes[0] >> 6
-            self._minor = (raw_bytes[0] & 0b00111000) >> 3
-            self._patch = raw_bytes[0] & 0b00000111
+            # region «Парсим» полученный байт и устанавливаем значения
+            self._major = raw_bytes[0] >> 6                    # Старшие два бита — «мажор»
+            self._minor = (raw_bytes[0] & 0b00111000) >> 3     # ... следующие три бита — «минор»
+            self._patch = raw_bytes[0] & 0b00000111            # ... младшие три бита — «патч»
+            # endregion
         else:
+            # region Поднимаем исключение
             raise ValueError(errs.PVS_ONLY_ONE_BYTE_EXPECTED)
+            # endregion
 
     def __repr__(self) -> str:
         """ Репрезентация (человеко-читаемое, наглядное представление объекта, который должен рассматриваться как
-        сдвиг по отношению к последней актуальной полной версии) """
+        смещение по отношению к последней актуальной «Полной версии») """
+        # region Готовим «элементы»
         descr = f'The Short Version (shifts) of protocol (instance of {__class__.__name__}) is {self.as_str()}'
-        ...  # todo ...
         mj = f'major shift: {self._major}'
         mn = f'minor shift: {self._minor}'
         pt = f'patch shift: {self._patch}'
+        # endregion
+        # region «Собираем» представление
         result = (f'{descr}:'
                   f'\n ▪️ {mj};'
                   f'\n ▪️ {mn};'
                   f'\n ▪️ {pt}.'
-                  f'\nAs binary string: {self.as_binary_str()}. As bytes (in hexadecimal notation): {self.as_hex()}.')
+                  f'\nAs binary string: {self.as_binary_str()}. '
+                  f'As bytes (in hexadecimal notation): {self.as_hex()}.')
+        # endregion
         return result
 
     def __str__(self) -> str:
         """ Человеко-читаемое строковое представление (например, для JSON) """
         return self.as_str()
 
-    def __lshift__(self, other):
-        """ Установка через << (на вход должен поступить кортеж из трех целых чисел, но с ограничениями:
-        2 бита на мажор, и по 3 бита на минор и на патч) """
+    def __lshift__(self, other: tuple[int, int, int]) -> None:
+        """ Установка через операцию сдвига влево «<<» (на вход должен поступить кортеж из трех целых чисел, но с
+        ограничениями: 2 бита на «мажор», и по 3 бита на «минор» и на «патч») """
+        # region Устанавливаем значения, путем пере-использования методов
         self.major, self.minor, self.patch = other
+        # endregion
 
-    # Метод __iadd__ для краткой версии не требуется
-
-    def _get_bytes(self):
+    def _get_bytes(self) -> int:
         """ Возвращает один байт (2 бита - мажор, 3 бита - минор, 3 бита - патч) """
-        mj = self._major << 6
-        mn = self._minor << 3
-        pt = self._patch
+        # region Собираем наш единственный байт
+        mj = self._major << 6                                  # Поднимаем «мажор» в старшие 2 бита
+        mn = self._minor << 3                                  # Поднимаем «минор» в следующие 3 бита
+        pt = self._patch                                       # Добавляем «патч» в последние 3 бита
+        # endregion
         return mj + mn + pt
 
     def as_str(self) -> str:
@@ -75,7 +114,8 @@ class ProtocolVersionShort:
         смещения для последней актуальной полной версии (2 бита - мажор, 3 бита - минор, 3 бита - патч) """
         return bytearray([self._get_bytes()])
 
-    def as_hex(self):
+    def as_hex(self) -> str:
+        """ Возвращает текущее значение в виде строки, состоящей из шестнадцатеричных символов (1 байт = 2 символа) """
         return self.as_bytes().hex()
 
     def as_binary_str(self) -> str:  # noqa
@@ -91,6 +131,8 @@ class ProtocolVersionShort:
         """ Установка значения смещения для "Мажора" (по отношению к последней актуальной полной версии) """
         if isinstance(value, int) and (0 <= value <= 3):
             self._major = value
+            if self._observer:
+                self._observer.process_changing()
         else:
             raise ValueError(errs.PVS_MAJOR_VALUE_ERROR)
 
@@ -104,6 +146,8 @@ class ProtocolVersionShort:
         """ Установка значения смещения для "Минора" (по отношению к последней актуальной полной версии) """
         if isinstance(value, int) and (0 <= value <= 7):
             self._minor = value
+            if self._observer:
+                self._observer.process_changing()
         else:
             raise ValueError(errs.PVS_MINOR_VALUE_ERROR)
 
@@ -117,8 +161,20 @@ class ProtocolVersionShort:
         """ Установка значения смещения для "Патча" (по отношению к последней актуальной полной версии) """
         if isinstance(value, int) and (0 <= value <= 7):
             self._patch = value
+            if self._observer:
+                self._observer.process_changing()
         else:
             raise ValueError(errs.PVS_PATCH_VALUE_ERROR)
+
+    @property
+    def observer(self) -> ProtocolVersionObserver:
+        return self._observer
+
+    @observer.setter
+    def observer(self, value: ProtocolVersionObserver) -> None:
+        if isinstance(value, ProtocolVersionObserver):
+            self._observer = value
+            self._observer.process_changing()
 
 
 class ProtocolVersionFull:
@@ -126,17 +182,27 @@ class ProtocolVersionFull:
 
     @dispatch(int, int, int)
     def __init__(self, major: int = 0, minor: int = 1, patch: int = 0) -> None:
-        """ Инициализация (установка начальных значений) через установку "мажора", "минора" и "патча" """
-        self._major: int = -1  # Признак того, что значение не установлено
+        """ Инициализация (установка начальных значений) через установку отдельно «мажора», «минора» и «патча» """
+        # region Устанавливаем «неопределенные» значения
+        self._major, self._minor, self._patch = _undefined_version()
+        # endregion
+        # region Обозначаем «наблюдателя»
+        self._observer = None
+        self._observer: ProtocolVersionObserver
+        # endregion
+        # region Устанавливаем значения с соответствующими проверками
         self.major = major
-        self._minor: int = -1  # Признак того, что значение не установлено
         self.minor = minor
-        self._patch: int = -1  # Признак того, что значение не установлено
         self.patch = patch
+        # endregion
 
     @dispatch(bytearray)
     def __init__(self, raw_bytes: bytearray) -> None:
         """ Инициализация через чтение "сырых" байтов (должен поступить байтовый массив из двух байтов) """
+        # region Обозначаем «наблюдателя»
+        self._observer = None
+        self._observer: ProtocolVersionObserver
+        # endregion
         if len(raw_bytes) == 2:
             self._major = raw_bytes[0]
             self._minor = raw_bytes[1] >> 4
@@ -213,6 +279,8 @@ class ProtocolVersionFull:
         """ Установка значения "Мажора" """
         if isinstance(value, int) and (0 <= value <= 255):
             self._major = value
+            if self._observer:
+                self._observer.process_changing()
         else:
             raise ValueError(errs.PVF_MAJOR_VALUE_ERROR)
 
@@ -226,6 +294,8 @@ class ProtocolVersionFull:
         """ Установка значения "Минора" """
         if self._minor_is_correct(value):
             self._minor = value
+            if self._observer:
+                self._observer.process_changing()
         else:
             raise ValueError(errs.PVF_MINOR_VALUE_ERROR)
 
@@ -239,25 +309,32 @@ class ProtocolVersionFull:
         """ Установка значения "Патча" """
         if isinstance(value, int) and (0 <= value <= 15):
             self._patch = value
+            if self._observer:
+                self._observer.process_changing()
         else:
             raise ValueError(errs.PVF_PATCH_VALUE_ERROR)
 
 
-class ProtocolVersion:
+class ProtocolVersion(ProtocolVersionObserver):
     """ «Фактическая» версия протокола — объект для хранения в памяти (не записывается в заголовки блоков) """
 
-    def __init__(self, full: ProtocolVersionFull, short: ProtocolVersionShort):
+    def __init__(self, full: ProtocolVersionFull, short: ProtocolVersionShort) -> None:
+        """
+
+        :param full: Полная версия (объект)
+        :param short: Краткая версия (объект)
+        """
         self._full = None
         self._full: ProtocolVersionFull
-        self.full = full
         self._short = None
         self._short: ProtocolVersionShort
+        self.full = full
         self.short = short
-        self._major, self._minor, self._patch = full + short
+        self._major, self._minor, self._patch = _undefined_version()
+        self._upd()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         descr = f'The Version of protocol (instance of {__class__.__name__}) is {self.as_str()}'
-        ...  # todo ...
         mj = f'major version: {self._major}'
         mn = f'minor version: {self._minor}'
         pt = f'patch version: {self._patch}'
@@ -271,9 +348,18 @@ class ProtocolVersion:
         """ Человеко-читаемое строковое представление (например, для JSON) """
         return self.as_str()
 
+    def _upd(self) -> None:
+        if self._full and self._short:
+            self._major, self._minor, self._patch = self._full + self._short
+        else:
+            self._major, self._minor, self._patch = _undefined_version()
+
     def as_str(self) -> str:
         """ Строковое представление (для JSON, или для словарного представления) """
         return f'{self._major}.{self._minor}.{self._patch}'
+
+    def process_changing(self):
+        self._upd()
 
     @property
     def major(self) -> int:
@@ -291,24 +377,21 @@ class ProtocolVersion:
         return self._patch
 
     @property
-    def short(self):
+    def short(self) -> ProtocolVersionShort:
         return self._short
 
     @short.setter
-    def short(self, value: ProtocolVersionShort):
+    def short(self, value: ProtocolVersionShort) -> None:
         if isinstance(value, ProtocolVersionShort):
             self._short = value
+            self._upd()
 
     @property
-    def full(self):
+    def full(self) -> ProtocolVersionFull:
         return self._full
 
     @full.setter
-    def full(self, value: ProtocolVersionFull):
+    def full(self, value: ProtocolVersionFull) -> None:
         if isinstance(value, ProtocolVersionFull):
             self._full = value
-
-
-ver = ProtocolVersion(ProtocolVersionFull(0, 1, 0), ProtocolVersionShort(0, 2, 0))
-print(ver)
-print(repr(ver))
+            self._upd()
--- tests/test_potocol_version.py
+++ tests/test_potocol_version.py
@@ -1,31 +1,144 @@
-from src.entities.protocol_version import ProtocolVersionFull
+from pytest import raises
+from src.entities import protocol_version as pver
 
 
-def test_positive_creation_as_integers():
-    v = ProtocolVersionFull(1, 2, 3)
+# region «Краткая версия»
+
+# region Позитивные тесты
+def test_p_short_version_init_via_ints():
+    # Определяем через int-ы
+    sv = pver.ProtocolVersionShort(1, 2, 3)
+    assert sv.minor == 2
+    assert sv.as_str() == '+1.+2.+3'
+    assert sv.as_binary_str() == '01010011'
+
+
+def test_p_short_version_init_via_bytes():
+    v = pver.ProtocolVersionShort(bytearray([0b01010011]))
+    assert v.as_hex() == '53'
+    assert v.major == 1
+
+
+def test_p_short_version_repr():
+    v = pver.ProtocolVersionShort(0, 0, 0)
+    assert str(v) == '+0.+0.+0'
+    assert repr(v)[:3] == 'The'  # Полное представление довольно длинное...
+
+
+def test_p_short_version_changing_values():
+    v = pver.ProtocolVersionShort(0, 0, 0)
+    v << (1, 2, 3)
+    assert v.as_str() == '+1.+2.+3'
+
+
+def test_p_short_version_changing_all():
+    v = pver.ProtocolVersionShort(0, 0, 0)
+    v.major += 1
+    v.minor += 2
+    v.patch += 3
+    assert v.major == 1
+    assert v.minor == 2
+    assert v.patch == 3
+
+
+def test_p_short_version_getting_bytes():
+    v = pver.ProtocolVersionShort(1, 2, 3)
+    assert v._get_bytes() == 83  # 01010011
+    assert v.as_bytes() == bytearray([83])
+
+# endregion
+
+
+# region Негативные тесты
+def test_n_short_version_init_via_ints():
+    # Определяем через int-ы
+    with raises(ValueError):
+        sv = pver.ProtocolVersionShort(5, 9, 12)
+
+
+def test_n_short_version_init_via_bytes():
+    arr = bytearray([1, 2, 3])
+    with raises(ValueError):
+        v = pver.ProtocolVersionShort(arr)
+
+
+def test_n_short_version_changing_values():
+    v = pver.ProtocolVersionShort(0, 0, 0)
+    with raises(ValueError):
+        v << (9, 9, 15)
+
+# endregion
+
+# endregion
+
+
+# region «Полная версия»
+
+# region Позитивные тесты
+def test_p_full_version_init_via_ints():
+    v = pver.ProtocolVersionFull(1, 2, 3)
     assert v.minor == 2
     assert str(v) == '1.2.3'
     assert v.as_binary_str() == '0000000100100011'
 
 
-def test_positive_creation_as_bytearray():
-    v = ProtocolVersionFull(1, 2, 3)
-    v1 = ProtocolVersionFull(v.as_bytes())
-    assert v1.patch == 3
-    assert str(v1) == '1.2.3'
-    assert v1.as_binary_str() == '0000000100100011'
-
-
-def test_positive_representation():
-    v = ProtocolVersionFull(5, 8, 13)
+def test_p_full_version_repr():
+    v = pver.ProtocolVersionFull(5, 8, 13)
     assert str(v) == '5.8.13'
     assert repr(v)[:3] == 'The'  # Полное представление довольно длинное...
     assert v.as_bytes() == b'\x05\x8d'
 
 
-def test_value_changing():
-    v = ProtocolVersionFull(5, 8, 13)
+def test_p_full_version_init_via_bytes():
+    v = pver.ProtocolVersionFull(1, 2, 3)
+    v1 = pver.ProtocolVersionFull(v.as_bytes())
+    assert v1.patch == 3
+    assert str(v1) == '1.2.3'
+    assert v1.as_binary_str() == '0000000100100011'
+
+
+def test_p_full_version_value_changing():
+    v = pver.ProtocolVersionFull(5, 8, 13)
     v.patch += 1
     assert v.patch == 14
     v << (7, 9, 14)
     assert v.major == 7
+
+# endregion
+
+# region Негативные тесты
+
+# endregion
+
+# endregion
+
+
+# region Фактическая версия (полная + кратка)
+
+# region Позитивные тесты
+def test_p_ver_init():
+    f_ver = pver.ProtocolVersionFull(0, 1, 0)
+    s_ver = pver.ProtocolVersionShort(0, 0, 0)
+    v = pver.ProtocolVersion(f_ver, s_ver)
+    assert v.minor == 1
+    v.full.patch += 1
+    assert v.full.patch == 1
+
+
+
+# endregion
+
+# region Негативные тесты
+
+# endregion
+
+# endregion
+
+# region Прочее
+def test_undefined_version():
+    assert pver._undefined_version() == (-1, -1, -1)
+
+
+# endregion
+
+
